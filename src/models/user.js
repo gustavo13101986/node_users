dbe37f27b1332dd1fade81a393bf2e4912c6c873
gustavo L <gustalondon2016@gmail.com>
2021-05-29 17:23:35 -0500

    add schema user

---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
+const { Schema } = mongoose
+
+const saltRounds = 10;
+
+const userSchema = new Schema({
+    correo: { type: String, required: true, unique: true },
+    nombre: { type: String, required: true },
+    apellido: { type: String, required: true },
+    telefono: { type: String, required: true },
+    password: { type: String, required: true },
+
+})
+
+userSchema.pre('save', (next) => {
+    if(this.isNew || this.isModiFied('password')){
+
+        const document = this;
+
+        bcrypt.hash(document.password, saltRounds, function(err, hash) {
+            if(err){
+                next(err)
+            }else{
+                document.password = hash
+                next()
+            }
+        });
+    }else{
+        next()
+    }
+})
+
+userSchema.methods.isCorrectPassword = function(password, callback){
+    bcrypt.compare(password, this.password, function(err, res){
+        if(err){
+            callback(err)
+        }else{
+            callback(err, res)
+        }
+    })
+}
+
+module.exports = mongoose.model('User', userSchema)
